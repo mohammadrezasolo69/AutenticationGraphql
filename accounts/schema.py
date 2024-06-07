9b786290d057e751d81f67dbfbd53f15d81c441e
Mohammadreza <mohammdrezasolo69@gmail.com>
2024-06-07 17:06:30 +0330

    :rocket: complete RequestOtp

--- accounts/schema.py
+++ accounts/schema.py
@@ -3,6 +3,10 @@ from django.contrib.auth import get_user_model
 import graphene
 from graphene_django import DjangoObjectType
 
+from utils.redis_connection import redis_set, redis_get
+from utils.generate_random_code import generate_random
+from utils.sender import sms_sender
+
 
 # ------------------------------------- Query -------------------------------------------------
 class Query(graphene.ObjectType):
@@ -19,11 +23,11 @@ class RequestOtp(graphene.Mutation):
 
     @staticmethod
     def mutate(root, info, phone_number):
-        opt_code = 123456
-        # send_sms()
+        opt_code = generate_random(length=6, use_digit=True)
+        redis_set(key=phone_number, data=str(opt_code), ex=120)
+        sms_sender(phone_number=phone_number, body=opt_code)
         return RequestOtp(ok=True, message='otp send .')
 
 
 class Mutation(graphene.ObjectType):
     request_otp = RequestOtp.Field()
-
