7ceee9985d4a4a9a7e144ca2ac5bbd7714ddee5f
Mohammadreza <mohammdrezasolo69@gmail.com>
2024-06-09 13:46:20 +0330

    :rocket: bugfix

--- accounts/services.py
+++ accounts/services.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
 from django.contrib.auth import get_user_model
+from accounts.selectors import get_user_by_phone_number
 
 
 def get_or_create_user(phone_number):
-    user, created = get_user_model().objects.get_or_create(
+    user = get_user_by_phone_number(phone_number=phone_number)
+    if user is not None:
+        return user, False
+    user = get_user_model().objects.create(
         phone_number=phone_number, is_verify=True, verify_date=datetime.now(), is_active=True)
-    return user, created
+    return user, True
